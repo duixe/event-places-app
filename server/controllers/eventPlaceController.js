2628ca6ba579b09f985ca021e016b6c08a4df7db
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-07-18 17:49:49 +0000

    added filtering to get places

--- server/controllers/eventPlaceController.js
+++ server/controllers/eventPlaceController.js
@@ -2,7 +2,21 @@ const Place = require('../models/eventPlaceModel');
 
 exports.getAllPlaces = async (req, res) => {
   try {
-    const eventPlaces = await Place.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedStrings = ['page', 'sort', 'limit', 'fields'];
+    excludedStrings.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Place.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const eventPlaces = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: eventPlaces.length,
@@ -48,23 +62,47 @@ exports.createPlace = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: err,
     });
   }
 };
 
-exports.updatePlace = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      place: '<Updated Place Here...>',
-    },
-  });
+exports.updatePlace = async (req, res) => {
+  const updatedEventPlace = await Place.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  try {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updatedEventPlace,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deletePlace = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deletePlace = async (req, res) => {
+  // not a good practise to send back the deleted doc
+  await Place.findByIdAndDelete(req.params.id);
+  try {
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(204).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- server/models/eventPlaceModel.js
+++ server/models/eventPlaceModel.js
@@ -6,26 +6,56 @@ const placeSchema = new mongoose.Schema({
     required: [true, 'An Event Place must have a name'],
     unique: true,
   },
+  maxSize: {
+    type: Number,
+    required: [true, 'Event place must maximum number of people'],
+  },
+  eventSuite: [String],
   description: {
     type: String,
     required: [true, 'An Event Place must have a description'],
+    trim: true,
+  },
+  summary: {
+    type: String,
+    required: [true, 'An Event Place must have a summarized description'],
+    trim: true,
   },
   capacity: {
     type: String,
   },
-  rating: {
+  duration: {
+    type: Number,
+  },
+  averageRatings: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'An Event place must have a price'],
   },
+  discount: Number,
   location: String,
   booked: {
     type: Boolean,
     default: false,
   },
+  coverImage: {
+    type: String,
+    required: [true, 'An event place must have a cover image'],
+  },
+  allImages: [String],
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  start_date: Date,
+  end_date: Date,
 });
 
 const Place = mongoose.model('Place', placeSchema);
