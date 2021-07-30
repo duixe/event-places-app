6381eb7215a8c40639ee842977bfff00a0c82256
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-07-30 00:34:54 +0000

    Refactor controllers to use Custom Error Handler

--- server/controllers/eventPlaceController.js
+++ server/controllers/eventPlaceController.js
@@ -1,6 +1,7 @@
 const Place = require('../models/eventPlaceModel');
 const APIFeatures = require('../utils/placesAPIFeature');
 const catchAsync = require('../utils/catchAsync');
+const ErrorSetter = require('../utils/ErrorSetter');
 
 exports.aliasTopPlaces = (req, res, next) => {
   req.query.limit = 10;
@@ -10,48 +11,43 @@ exports.aliasTopPlaces = (req, res, next) => {
   next();
 };
 
-exports.getAllPlaces = async (req, res) => {
-  try {
-    // EXECUTE QUERY
-    const features = new APIFeatures(Place.find(), req.query)
-      .filter()
-      .sort()
-      .specificFields()
-      .paginate();
-    const eventPlaces = await features.query;
+exports.getAllPlaces = catchAsync(async (req, res, next) => {
+  // EXECUTE QUERY
+  const features = new APIFeatures(Place.find(), req.query)
+    .filter()
+    .sort()
+    .specificFields()
+    .paginate();
+  const eventPlaces = await features.query;
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: eventPlaces.length,
-      data: {
-        eventPlaces,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: eventPlaces.length,
+    data: {
+      eventPlaces,
+    },
+  });
+});
 
-exports.getPlace = async (req, res) => {
-  try {
-    const eventPlace = await Place.findById(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        eventPlace,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
+exports.getPlace = catchAsync(async (req, res, next) => {
+  const eventPlace = await Place.findById(req.params.id);
+
+  if (!eventPlace) {
+    return next(
+      new ErrorSetter(
+        "The event place you're looking for could not be found",
+        404
+      )
+    );
   }
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      eventPlace,
+    },
+  });
+});
 
 exports.createPlace = catchAsync(async (req, res, next) => {
   const newEventPlace = await Place.create(req.body);
@@ -64,87 +60,85 @@ exports.createPlace = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updatePlace = async (req, res) => {
-  try {
-    const updatedEventPlace = await Place.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
+exports.updatePlace = catchAsync(async (req, res, next) => {
+  const updatedEventPlace = await Place.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!updatedEventPlace) {
+    return next(
+      new ErrorSetter(
+        "The event place you're looking for could not be found",
+        404
+      )
     );
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updatedEventPlace,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
   }
-};
 
-exports.deletePlace = async (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedEventPlace,
+    },
+  });
+});
+
+exports.deletePlace = catchAsync(async (req, res, next) => {
   // not a good practise to send back the deleted doc
-  await Place.findByIdAndDelete(req.params.id);
-  try {
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(204).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+  const deletedPlace = await Place.findByIdAndDelete(req.params.id);
 
-exports.getPlaceStats = async (req, res) => {
-  try {
-    const stats = await Place.aggregate([
-      {
-        $match: { averageRatings: { $gte: 4 } },
-      },
-      {
-        $group: {
-          // _id: null,
-          _id: '$averageRatings',
-          numOfPlaces: { $sum: 1 },
-          numRatings: { $sum: '$ratingsQuantity' },
-          avgRating: { $avg: '$averageRatings' },
-          avgPrice: { $avg: 'price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' },
-        },
-      },
-      // {
-      //   $project: {
-      //     _id: 0,
-      //   },
-      // },
-      {
-        // sort the aggregations by {avgPrice or avgRating..etc} key stated above in ascending order (1)
-        // $sort: { avgRating: 1 },
-        $sort: { avgPrice: 1 },
-      },
-    ]);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats,
-      },
-    });
-  } catch (error) {
-    res.status(204).json({
-      status: 'fail',
-      message: error,
-    });
+  if (!deletedPlace) {
+    return next(
+      new ErrorSetter(
+        "The event place you're looking for could not be found",
+        404
+      )
+    );
   }
-};
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.getPlaceStats = catchAsync(async (req, res, next) => {
+  const stats = await Place.aggregate([
+    {
+      $match: { averageRatings: { $gte: 4 } },
+    },
+    {
+      $group: {
+        // _id: null,
+        _id: '$averageRatings',
+        numOfPlaces: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$averageRatings' },
+        avgPrice: { $avg: 'price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+      },
+    },
+    // {
+    //   $project: {
+    //     _id: 0,
+    //   },
+    // },
+    {
+      // sort the aggregations by {avgPrice or avgRating..etc} key stated above in ascending order (1)
+      // $sort: { avgRating: 1 },
+      $sort: { avgPrice: 1 },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats,
+    },
+  });
+});
