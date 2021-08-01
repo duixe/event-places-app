39693d3607df701d739e1a2aab09b1ea506ef289
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-08-01 19:21:34 +0000

    finished dev and prod error handling

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -1,9 +1,37 @@
+const sendDevErrors = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendProdErrors = (err, res) => {
+  // error caused by a client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // error cause by a bug
+  } else {
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'something went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // amount of errors to expose based on the env
+  if (process.env.NODE_ENV === 'development') {
+    sendDevErrors(err, res);
+  } else if (process.env.NODE_ENV === 'development') {
+    sendProdErrors(err, res);
+  }
 };
