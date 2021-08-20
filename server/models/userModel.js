8277820f501d1cf9bba10b04697db6acf39f5bf1
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-08-20 21:46:00 +0000

    completed password reset and change password functionality

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -74,6 +74,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
 userSchema.methods.PasswordchangedAfter = function (jwtTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const guard = require('../middleware/guardRoutes');
 
 const router = express.Router();
 
 //Auth routes
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+
 router.post('/forgot-password', authController.forgotPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
+
+router.patch(
+  '/change-password',
+  guard.guardRoute,
+  authController.changePassword
+);
 
 router
   .route('/')
