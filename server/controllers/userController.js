b525ebfee0cbf7214eedb85a58525fe25fdfe4fc
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-08-23 22:18:17 +0000

    completed account deactivation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -84,3 +84,12 @@ exports.updateProfile = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.deactivateAccount = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
